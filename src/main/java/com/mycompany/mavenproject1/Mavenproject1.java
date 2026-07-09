2a2bfd0e505e9f117727bc6820bedf084c92421e
CL2-PC <CL2-PC@CL2-PC27>
2026-07-09 09:06:07 +0800

    ACT2B first program push to git

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+import java.util.Scanner;
+
+/**
+ *
+ * @author CL2-PC
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        int sum, diff;
+        Scanner input = new Scanner(System.in);
+        System.out.println( "Enter a number: ");
+        int x = input.nextInt();
+        System.out.println( "Enter a number: ");
+        int y = input.nextInt();
+        
+        sum = x + y;
+        
+        System.out.println(""+sum);
+        
+        
+    }
+}
